5dcbeadf516f1dee9a0739e9333913f8c4887bfe
shashwat2003 <shashwat13.8@gmail.com>
2025-02-21 15:09:26 +0530

    fix(docs): fixed `vite.config.ts`

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,30 +1,20 @@
-import type { UserConfig } from 'vite'
-import { one } from 'one/vite'
-import { tamaguiPlugin } from '@tamagui/vite-plugin'
+import { tamaguiPlugin } from "@tamagui/vite-plugin";
+import { one } from "one/vite";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
   plugins: [
     one({
-      react: {
-        compiler: process.env.NODE_ENV === 'production',
-      },
-
       web: {
-        defaultRenderMode: 'ssg',
-      },
-
-      native: {
-        // set to the key of your native app
-        // will call AppRegistry.registerComponent(app.key)
-        key: 'one-example',
+        defaultRenderMode: "spa",
       },
     }),
 
     tamaguiPlugin({
-      optimize: process.env.NODE_ENV === 'production',
-      components: ['tamagui'],
-      config: './src/tamagui/tamagui.config.ts',
-      outputCSS: './src/tamagui/tamagui.css',
+      optimize: true,
+      config: "./src/tamagui/config",
+      components: ["tamagui"],
+      outputCSS: "./src/tamagui/tamagui.css",
     }),
   ],
-} satisfies UserConfig
+});
